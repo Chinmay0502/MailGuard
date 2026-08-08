07de55e64cefb12e3aaea72837948cf3611f6a4f
Chinmay0502 <chinamayakumarsahoo@gmai.com>
2026-08-08 22:04:56 +0530

    Fix all production apis

--- backend/src/server.js
+++ backend/src/server.js
@@ -44,8 +44,8 @@ app.use((req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
