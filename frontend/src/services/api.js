9137a0bc2f5bcdeb3c19f2c2617ed902e6fe5819
Chinmay0502 <chinamayakumarsahoo@gmai.com>
2026-08-08 22:18:49 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://spam-mail-backend.onrender.com",
+  baseURL: "https://spam-mail-ml.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
